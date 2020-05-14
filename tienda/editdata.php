3bb790fdbd113b6217b96b9eddddd2f9c4857b69
Franklin-Molina <asdfglxl7@gmail.com>
2020-05-14 00:02:34 -0500

    sql

--- tienda/editdata.php
+++ tienda/editdata.php
@@ -5,9 +5,12 @@
 	$id = $_POST['id'];
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	$nivel = $_POST['nivel'];
+	$telefono = $_POST['telefono'];
+	$ciudad = $_POST['ciudad'];
+	$direccion = $_POST['direccion'];
 
 	
-	$connect->query("UPDATE usuarios SET username='".$username."', password='".$password."', nivel='".$nivel."' WHERE id=". $id);
+	
+	$connect->query("UPDATE usuarios SET username='".$username."', password='".$password."', telefono='".$telefono."', ciudad='".$ciudad."', direccion='".$direccion."'   WHERE id=". $id);
 
 ?>
